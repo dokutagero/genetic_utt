2cb6b35f647a5739637055c20b48606b70c78984
dokutagero <s16211@student.dtu.dk>
2017-03-13 21:17:05 +0100

    Last implementation

--- app/GeneticAlgorithmPureTT.py
+++ app/GeneticAlgorithmPureTT.py
@@ -1,5 +1,6 @@
 # import GeneticAlgorithmBase
 import numpy as np
+import time
 
 class GeneticAlgorithmPureTT():
 
@@ -12,12 +13,12 @@ class GeneticAlgorithmPureTT():
         self.population = self.initialization(data["basics"]["rooms"],
                         data["basics"]["periods_per_day"] * data["basics"]["days"])
 
-
+        self.print_population()
 
 
     def initialization(self, num_rooms, timeslots):
         population = []
-
+        finish_time = time.time() + 60*1
         for i in range(self.population_size):
             individual = np.zeros(shape=(num_rooms, timeslots), dtype=np.int8) - 1
             idcs = [idcs for idcs, val in np.ndenumerate(individual)]
@@ -30,11 +31,13 @@ class GeneticAlgorithmPureTT():
 
             idx_conflict = self.test_feasibility(individual)
             while idx_conflict != None:
+                # if finish_time < time.time():
+                #     population.append(individual)
+                #     return population
                 individual = self.random_swap(idx_conflict, individual)
                 idx_conflict = self.test_feasibility(individual)
 
             population.append(individual)
-
         return population
 
     def evaluation(self, fitness_func):
@@ -58,18 +61,19 @@ class GeneticAlgorithmPureTT():
 
         tmp = individual[(row,col)]
         individual[(row,col)] = individual[idx_conflict]
-        print 'miau'
-        print idx_conflict
-        print individual[idx_conflict]
+        # print 'miau'
+        # print idx_conflict
+        # print individual[idx_conflict]
         individual[idx_conflict] = tmp
-        print 'swapped'
-        print individual[:, idx_conflict[1]]
+        # print 'swapped'
+        # print individual[:, idx_conflict[1]]
         return individual
 
 
 
     def test_feasibility(self, individual):
 
+        # Unavailability
         for course, constraints in self.data["unavailability"].iteritems():
             course_no = int(course[1:])
             for idx in zip(constraints["day"], constraints["period"]):
@@ -77,8 +81,37 @@ class GeneticAlgorithmPureTT():
                 # print individual.shape
                 conflict_timeslot = (self.data["basics"]["periods_per_day"])*idx[0]+idx[1]
                 if course_no in individual[:,conflict_timeslot]:
-                    print course_no
+                    # print course_no
                     # print individual[:, conflict_timeslot]
                     room_idx = np.where(individual[:, conflict_timeslot] == course_no)[0][0]
-                    print individual[room_idx, conflict_timeslot]
+                    # print individual[room_idx, conflict_timeslot]
                     return (room_idx, conflict_timeslot)
+
+
+        # Curricula
+        for num_timeslot in range(individual.shape[1]):
+            for curricula, courses in self.data["relations"].iteritems():
+                # courses_tmp = courses
+                row_curricula_conflict = []
+                for course in courses:
+                    # print 'holaaaaa'
+                    # print courses
+                    # print course[1:]
+                    row_curricula_conflict.extend(np.where(individual[:,num_timeslot] == int(course[1:]))[0])
+                    if len(row_curricula_conflict) > 1:
+                        # print 'row curricula'
+                        # print row_curricula_conflict[-1]
+                        return (row_curricula_conflict[-1], num_timeslot)
+
+
+    def print_population(self):
+
+        for course in self.data["courses"].keys():
+            course_room, course_tslots = np.where(self.population[0] == int(course[1:]))
+            days = [day/6 for day in course_tslots]
+            periods = [day%6 for day in course_tslots]
+
+
+            with open('first_output.sol', 'a') as outputfile:
+                for day, period, room in zip(days, periods, course_room):
+                    print >> outputfile, course, day, period, 'R'+'%04d' % room
--- memetic_algo.py
+++ memetic_algo.py
@@ -11,7 +11,7 @@ runtime = sys.argv[-1]
 data = load_data.load(sys.argv[1:-1])
 #
 mutation_prob = 0.001
-pop_size = 10
+pop_size = 1
 ga = GeneticAlgorithmPureTT(data, pop_size, mutation_prob)
 
 
