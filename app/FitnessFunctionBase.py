8d1e297bf8b28cdca5844ce39960e7aee70025a8
dokutagero <s16211@student.dtu.dk>
2017-03-14 10:17:09 +0100

    Added docstring for the fitness function class

--- app/FitnessFunctionBase.py
+++ app/FitnessFunctionBase.py
@@ -2,6 +2,15 @@ from abc import ABCMeta, abstractmethod
 
 class FitnessFunctionBase(object):
 
+    """Parent class for dealing with fitness functions and constraint checkers.
+
+
+    Attributes:
+        data {dict}: Single dictionary containing the data used in a particular
+                        problem..
+
+    """
+
     __metaclass__ = ABCMeta
 
     def __init__(self, data):
